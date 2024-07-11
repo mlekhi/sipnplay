ebde8d2a0cb2ae942fb8764d79c6708f6b25fbe5
Maya <maya.lekhi1@gmail.com>
2024-07-11 12:44:03 -0400

    creating main pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Menu from "./components/Menu/Menu.js";
+import Events from "./components/Events/Events.js";
+import About from "./components/About/About.js";
+import Landing from "./components/Landing/Landing.js";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Menu />
+      <div className="body">
+        <div className="viewer">
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/menu" component={Menu} />
+            <Route exact path="/events" component={Events} />
+            <Route exact path="/about" component={About} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/pages/About/About.js
@@ -0,0 +1,1 @@
+// include carousel with board game selection

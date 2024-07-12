105c187c538dd2edc1766ca5711cb4109784697e
Jeffrey Zhang <109628105+JeffreyZhangsd@users.noreply.github.com>
2024-07-11 19:42:00 -0700

    remove examples from landing page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
-import Examples from "../../components/Canvas/Examples";
 
 function Landing() {
   return (
     <div style={{ display: "flex" }}>
       <p>text</p>
-      <Examples />
     </div>
   );
 }

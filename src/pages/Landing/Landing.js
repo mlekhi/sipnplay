c8623175165bdb5bd8bbe1172ad694359e481587
RheaKapadia <rheakapadia@gmail.com>
2024-07-12 11:00:50 -0400

    added comments

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -7,9 +7,12 @@ function Landing() {
   return (
     <div>
       <div
+      // hero section of landing page, add background gradient image
         className="App-header2 bg-cover bg-center"
         style={{ backgroundImage: "url('backgroundimage.png')" }}>
+          {/* add 3d logo */}
         <img fetchpriority="high" src="logo512.png" className="logo" alt="Sip & Play Logo" />
+        {/* add 3d dice models with various rotations for diversity */}
         <div id="dice" className="dice">
           <Dice rotate={[1, 1, 1]} />
           <Dice rotate={[1, -1, 1]} />
@@ -17,6 +20,7 @@ function Landing() {
           <Dice rotate={[1, 1, -1]} />
         </div>
       </div>
+      {/* below hero section with more info about sipnplay */}
       <div className="App-header">
         <p>Come on in with your friends and family. Play from our collection of over </p>
         <span className="bold-words">500+ games for just $10/person!</span>
@@ -25,6 +29,8 @@ function Landing() {
         <span className="bold-words">bubble tea, coffee, beer, sandwiches or salads!</span>
         <p>If you want to guarantee a table, you can make a</p>
         <span className="bold-words">reservation for $15 a person!</span>
+
+        {/* add 3d coffee model */}
         <Coffee />
       </div>
     </div>
